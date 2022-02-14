982e5edafbdd593aac26253811908ae0cd459390
Nazmul Hussain <nazmul.hussain@streamstech.com>
2022-02-14 13:54:19 +0600

    [Phase-4] add local storage service

---
+++ src/app/core/services/api/local-storage.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  public setItem(key: string, data: any): void {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+
+  public getItem( key: string ): any {
+    const item = localStorage.getItem(key);
+    return item && JSON.parse(item)
+      ? JSON.parse(item).data
+      : null;
+  }
+
+  public clearAll(): void {
+    localStorage.clear();
+  }
+
+  public clear(key: string): void {
+    localStorage.removeItem(key);
+  }
+}
