d04985153aa1835c48fc064cce07096294a1dd0f
Nazmul Hussain <nazmul.hussain@streamstech.com>
2022-02-16 08:23:30 +0600

    [Nice-to-have] init Unit Test error fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,21 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/modules/material.module';
+import { SharedUiModule } from './shared/modules/shared-ui.module';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MaterialModule,
+        SharedUiModule,
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +28,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cmm'`, () => {
+  it(`should have as title 'Movie Mania'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cmm');
+    expect(app.title).toEqual('Movie Mania');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cmm app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Movie Mania app is running!');
   });
 });
