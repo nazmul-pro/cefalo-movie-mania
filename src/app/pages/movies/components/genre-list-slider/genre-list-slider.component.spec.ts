25524e4c0fbf84f0fdc467beb5e7bd7ec2c4b3cc
Nazmul Hussain <nazmul.hussain@streamstech.com>
2022-02-16 14:20:30 +0600

    [Nice-to-have] fix genre slider component spec

--- src/app/pages/movies/components/genre-list-slider/genre-list-slider.component.spec.ts
+++ src/app/pages/movies/components/genre-list-slider/genre-list-slider.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from 'src/app/shared/modules/material.module';
+import { SharedUiModule } from 'src/app/shared/modules/shared-ui.module';
 
 import { GenreListSliderComponent } from './genre-list-slider.component';
 
@@ -8,7 +13,14 @@ describe('GenreListSliderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GenreListSliderComponent ]
+      declarations: [ GenreListSliderComponent ],
+      imports: [        
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MaterialModule,
+        SharedUiModule,
+        BrowserAnimationsModule,
+      ]
     })
     .compileComponents();
   });
@@ -20,6 +32,7 @@ describe('GenreListSliderComponent', () => {
   });
 
   it('should create', () => {
+    component.genres = [{id: 1, imageUrl: '', name: ''}];
     expect(component).toBeTruthy();
   });
 });
